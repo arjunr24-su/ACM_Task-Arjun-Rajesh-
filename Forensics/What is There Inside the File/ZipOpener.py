1dc7689eb72399bb5b945c56f5597edde1b98f08
arjunr24-su <arjunrajesh59@gmail.com>
2024-09-19 00:44:23 +0530

    Labs ongoing

--- Forensics/What is There Inside the File/ZipOpener.py
+++ Forensics/What is There Inside the File/ZipOpener.py
@@ -1,20 +1,16 @@
 import os
 import pyzipper
 
-# Path to your ZIP file
-zip_file_path = r'C:\Users\ARJUN RAJESH\OneDrive\Desktop\ACM_Task-Arjun-Rajesh-\ACM_Task-Arjun-Rajesh-\Forensics\What is There Inside the File\Trybreakingme.zip'
-# Password for the encrypted ZIP file
-password = b'42800643192346137370256222640646094543767840305996925'
 
-# Open the ZIP file in read mode
+zip_file_path = r'C:\Users\ARJUN RAJESH\OneDrive\Desktop\ACM_Task-Arjun-Rajesh-\Forensics\What is There Inside the File\Trybreakingme.zip'
+password = b''
+
+
 with pyzipper.AESZipFile(zip_file_path, 'r') as zip:
-    # Set the password
     zip.setpassword(password)
     
-    # List all the contents of the ZIP file
+ 
     zip_contents = zip.namelist()
-
-    # Extract all the contents of the ZIP file to the current directory
     for file in zip_contents:
         try:
             zip.extract(file, pwd=password)
@@ -22,6 +18,6 @@ with pyzipper.AESZipFile(zip_file_path, 'r') as zip:
         except RuntimeError as e:
             print(f"Failed to extract {file}: {e}")
 
-# Print the contents
+
 for file in zip_contents:
     print(file)
